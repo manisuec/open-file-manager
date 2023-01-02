08b3b8021cc5389661357ff6b74759c2bc2996b9
Manish Prasad <manishvaishu25@gmail.com>
2023-01-02 18:49:05 +0530

    updated layout

--- ofm-ui/src/components/OFM/OFMLayout.tsx
+++ ofm-ui/src/components/OFM/OFMLayout.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 
+import AppBar from './../layout/AppBar';
 import { OFMProps } from "./OFM.types";
 
 import "./ofm.scss";
 
 const OFMLayout: React.FC<OFMProps> = ({ heading, content }) => (
-  <div data-testid="test-component" className="test-component">
-    <h1 data-testid="test-component__heading" className="heading">
-      {heading}
-    </h1>
-    <div data-testid="test-component__content">{content}</div>
+  <div>
+    <AppBar>
+    </AppBar>
+    <div data-testid="test-component" className="test-component">
+      <h1 data-testid="test-component__heading" className="heading">
+        {heading}
+      </h1>
+      <div data-testid="test-component__content">{content}</div>
+    </div>
   </div>
 );
 
